41f1307d13432ab8e5c4fd0b062666ba98468761
Priyanka-MP <priyanka.mp2731@gmail.com>
2025-05-06 12:53:54 +0530

    Host-displayed

--- Application/main.py
+++ Application/main.py
@@ -1,12 +1,10 @@
 from fastapi import FastAPI
-from app.routes import health
+from Application.routes import health  # <- This matters!
 
 app = FastAPI(title="MechMate Roadside Assistance App")
 
-# Include routes
 app.include_router(health.router)
 
-# Root route (optional)
 @app.get("/")
 def read_root():
     return {"message": "Welcome to MechMate API"}
